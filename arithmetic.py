7012e7474969dbeb41422743666cccdd72784698
Seale Wong <seale.wong@gmail.com>
2021-02-25 01:39:45 +0000

    Added +,-,*,/ operators

--- arithmetic.py
+++ arithmetic.py
@@ -1,35 +1,51 @@
 """Functions for common math operations."""
 
-
 def add(num1, num2):
     """Return the sum of the two inputs."""
 
-    return 10
+    sum = num1 + num2
+    return(sum)
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
+    diff = num1 - num2
+    return diff 
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
+    product = num1 * num2
+    return product
+   
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
-
+    div = num1 / num2
+    return div
+ 
 def square(num1):
     """Return the square of the input."""
 
+    square = num1 * num1
+    return square
 
 def cube(num1):
     """Return the cube of the input."""
-
+    
+    cube = num1 * num1 * num1
+    return cube 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    power = num1 ** num2
+    return power
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    
+    mod = num1 % num2
+    return mod
--- calculator.py
+++ calculator.py
@@ -4,4 +4,29 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+while True:
+    operation = input("Enter your equation > ") # + 1 2
+    operation_list = operation.split(" ") # gives a list [+, 1, 2]
+    if operation_list[0] == "q":
+        break 
+    elif operation_list[0] == "+":
+        print(add(int(operation_list[1]), int(operation_list[2])))
+    elif operation_list[0] == "-":
+        print(subtract(int(operation_list[1]), int(operation_list[2])))
+    elif operation_list[0] == "*":
+        print(multiply(int(operation_list[1]), int(operation_list[2])))
+    elif operation_list[0] == "/":
+        print(divide(int(operation_list[1]), int(operation_list[2])))
+    
+
+
+# repeat forever:
+#     read input
+#     tokenize input
+#         if the first token is "q":
+#             quit
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
+#             (...etc.)
